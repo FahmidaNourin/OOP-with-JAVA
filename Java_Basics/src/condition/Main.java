974040028c01c32d82d94d61ffa31d26a8fe58b4
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2021-09-19 20:09:15 +0600

    Create Main.java

---
+++ Java_Basics/src/condition/Main.java
@@ -0,0 +1,30 @@
+package condition;
+import java.util.*;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner rj=new Scanner(System.in);
+		int a;
+		System.out.println("value of a");
+		a=rj.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("The number is even");
+		}
+		else
+		{
+			System.out.println("The number is odd");
+			
+		}
+		
+		
+		System.out.println("This Class is for Condition");
+		
+		
+
+	}
+
+}
