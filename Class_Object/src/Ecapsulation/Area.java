ca52dc4e375feb5f446c890862189ecf074b3d4c
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2021-10-04 20:47:57 +0600

    update

--- Class_Object/src/Ecapsulation/Area.java
+++ Class_Object/src/Ecapsulation/Area.java
@@ -2,23 +2,45 @@ package Ecapsulation;
 
 public class Area {
 	
+	
 	static private double height;
 	static private double base;
+	static private double return1;
+	static private double return2;
+	static private double redius;
 	
-	
-	Area(double h, double b){
+	static void setArms(double h, double b) {
 		height=h;
 		base=b;
 	}
 	
-	
-	
-	static double Triangle() {
+	static void Triangle() {
 		double area;
 		area = (height * base) / 2;
-		return (area);
+	    return1=area;  	   
 	}
-
-
+	
+	static double getTri() {
+		return return1;
+	}
+	
+	
+	
+	
+	
+	static void setRadius(double r) {
+		redius=r;
+	}
+	
+	
+	static void Circle() {
+		double area;
+		area = 3.1416*redius*redius;
+	    return2=area;  	   
+	}
+	
+	static double getCir() {
+		return return2;
+	}
 	
 }
--- Class_Object/src/Ecapsulation/Main.java
+++ Class_Object/src/Ecapsulation/Main.java
@@ -4,11 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Area n=new Area(1.2,42);
-		double tri2= n.Triangle();
+		Area n=new Area();
+		
+		n.setArms(1.2, 0.4);
+		n.Triangle();
+		double tri2=n.getTri();
 		System.out.println(tri2);
-		double tri= n.Triangle();
-		System.out.println(tri);
+		
+
+		
 	}
 
 }
