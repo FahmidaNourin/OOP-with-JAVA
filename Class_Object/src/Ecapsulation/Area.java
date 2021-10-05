49ae4ca48198c496392dbf7ddb88bb0660e67644
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2021-10-05 11:54:14 +0600

    rafsan

--- Class_Object/src/Ecapsulation/Area.java
+++ Class_Object/src/Ecapsulation/Area.java
@@ -1,46 +1,55 @@
 package Ecapsulation;
 
 public class Area {
-	
-	
-	static private double height;
-	static private double base;
-	static private double return1;
-	static private double return2;
-	static private double redius;
-	
-	static void setArms(double h, double b) {
-		height=h;
-		base=b;
+
+	static private double height;  // instence variable for height
+	static private double base;    // instence variable for base
+	static private double return1; // instence variable for returning area of triangle
+	static private double return2; // instence variable for returning area of circle
+	static private double redius;  // instence variable for radius
+    static private double arms;//instence
+    static private double return3;
+    
+	static void setArms(double h, double b) { 
+		height = h;
+		base = b;
 	}
-	
+
 	static void Triangle() {
 		double area;
 		area = (height * base) / 2;
-	    return1=area;  	   
+		return1 = area;
 	}
-	
+
 	static double getTri() {
 		return return1;
 	}
-	
-	
-	
-	
-	
+
 	static void setRadius(double r) {
-		redius=r;
+		redius = r;
 	}
-	
-	
+
 	static void Circle() {
 		double area;
-		area = 3.1416*redius*redius;
-	    return2=area;  	   
+		area = 3.1416 * redius * redius;
+		return2 = area;
 	}
-	
+
 	static double getCir() {
 		return return2;
 	}
 	
+     static void setArms(double a) {
+    	 arms=a;
+    	 
+     }
+     static void Square() {
+    	 double area;
+    	 area=arms*arms;
+    	 return3=area;
+     }
+     static double getSqr() {
+    	 return return3;
+     
+     }
 }
--- Class_Object/src/Ecapsulation/Main.java
+++ Class_Object/src/Ecapsulation/Main.java
@@ -3,16 +3,18 @@ package Ecapsulation;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Area n=new Area();
-		
+
+		Area n = new Area();
 		n.setArms(1.2, 0.4);
 		n.Triangle();
-		double tri2=n.getTri();
+		double tri2 = n.getTri();
 		System.out.println(tri2);
 		
-
+		n.setArms(77,77);
+		n.Square();
+		double sqr=n.getSqr();
+		System.out.println(sqr);
 		
-	}
 
+	}
 }
